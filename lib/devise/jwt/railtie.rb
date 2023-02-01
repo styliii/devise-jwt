253facfc0e9d81db71d7033549bc22784a156a67
Li Ouyang <li.ouyang@gmail.com>
2023-02-01 10:05:39 -0500

    typo

--- lib/devise/jwt/railtie.rb
+++ lib/devise/jwt/railtie.rb
@@ -7,7 +7,7 @@ module Devise
     # Pluck to rails
     class Railtie < Rails::Railtie
       initializer 'devise-jwt-middleware' do |app|
-        app.middleware.insert_after Warden::Manager
+        app.middleware.insert_after Warden::Manager, Warden::JWTAuth::Middleware
 
         config.after_initialize do
           Rails.application.reload_routes!
